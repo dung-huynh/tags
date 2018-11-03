ed48ede386df33c7cc57ced7b10448a920248e6c
Henok Seifu <hseifu@jacobs-university.de>
2018-11-03 23:58:38 +0100

    Allows download of videos and output of words

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import requests
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import jinja2
+import scenes_to_json
+
 
 jinja_environment = jinja2.Environment(autoescape=True, loader=jinja2.FileSystemLoader('templates'))
 
@@ -16,22 +18,22 @@ def home():
 
 @app.route('/',methods = ['POST'])
 def form():
-    video_url = request.form['text']
+    video_url = request.form['link']
     r = requests.get(video_url)
     video_file = video_url.split('/')[-1]
     #yield "Downloading file: "  + video_file + '\n'
     if video_file.split('.')[-1] not in extensions:
-        return "Invalid file format"
+       return "Invalid file format"
     # create response object 
     r = requests.get(video_url, stream = True) 
         
     # download started 
-    with open(video_file, 'wb') as f: 
+    with open("video" + video_file.split('.')[-1], 'wb') as f: 
         for chunk in r.iter_content(chunk_size = 1024*1024): 
             if chunk: 
                 f.write(chunk) 
-        
-    return render_template('output.html')
+    
+    return render_template('output.html', data = scenes_to_json.trial2(), link = video_url)
 
 app.run(debug = True)
 
