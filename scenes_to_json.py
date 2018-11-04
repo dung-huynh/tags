cec792dc2398c76da9f7ce0bbe294b20c50c801d
Henok Seifu <hseifu@jacobs-university.de>
2018-11-04 03:05:28 +0100

    beautified output, commented

--- scenes_to_json.py
+++ scenes_to_json.py
@@ -1,15 +1,17 @@
+#calls function that returns a list of scene objects 
+#Outputs dictonary of scenes mapped to containing words
 import scene_separator
 
-
-
+#get list returns dict
 def convert(scenes):
     output = {}
     counter = 1
     for scene in scenes:
-        output['scene' + str(counter)] = list(scene.words.keys())
+        output['Scene ' + str(counter)] = list(scene.words.keys())
         counter += 1
     return output
 
+#Calls function that returns list of scene object from video file
 def trial2():
     scenes = scene_separator.trial()
     a = convert(scenes)
